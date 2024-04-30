7cc6ba02d6353a2a9a513da1c98c364ed800940a
KimSungGyun <egoksg@osstem.com>
2024-04-30 10:03:06 +0900

    Service에서 DTO로 유효성 검사

---
+++ src/main/java/com/spring/validation/service/ContactService.java
@@ -0,0 +1,14 @@
+package com.spring.validation.service;
+
+import com.spring.validation.model.CreateContact;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Validated // 여기에 추가(AOP에서 처리)
+@Service
+public class ContactService {
+    public void createContact(@Valid CreateContact createContact) { // '@Valid'가 설정된 메서드가 호출될 때 유효성 검사를 진행한다.
+        // Do Something
+    }
+}
--- src/test/java/com/spring/validation/model/CreateContactTests.java
+++ src/test/java/com/spring/validation/model/CreateContactTests.java
@@ -1,9 +1,12 @@
 package com.spring.validation.model;
 
 import com.spring.validation.enums.ContactType;
+import com.spring.validation.service.ContactService;
 import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -11,8 +14,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Collection;
 import java.util.Locale;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootTest
 class CreateContactTests {
+    @Autowired
+    private ContactService contactService;
+
     @Test
     void test_validate() {
         Locale.setDefault(Locale.US);
@@ -28,6 +40,31 @@ class CreateContactTests {
 
         // When
         final Collection<ConstraintViolation<CreateContact>> constraintViolations = validator.validate(createContact);
+
+        // Then
+        assertEquals(1, constraintViolations.size()); // ConstraintViolation에 실패에 대한 정보가 담긴다.
+        assertEquals("must not be blank", constraintViolations.iterator().next().getMessage());
+    }
+
+    @Test
+    void test_serviceValidate() {
+        Locale.setDefault(Locale.US);
+        // Given
+        final CreateContact createContact = CreateContact
+                .builder()
+                .uid(null) // @NotBlank가 정의되어 있기때문에 null이 오면 안된다.
+                .contact("000")
+                .contactType(ContactType.PHONE_NUMBER)
+                .build();
+
+
+        // When
+        Set<ConstraintViolation<?>> constraintViolations = null;
+        try {
+            contactService.createContact(createContact);
+        } catch (ConstraintViolationException exception) {
+            constraintViolations = exception.getConstraintViolations();
+        }
 
         // Then
         assertEquals(1, constraintViolations.size()); // ConstraintViolation에 실패에 대한 정보가 담긴다.
